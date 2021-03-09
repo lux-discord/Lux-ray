95e4b9c00d34ae42ccfd32c98388760dc622d822
Euxcbsks <hawhaw02030746@gmail.com>
2021-03-09 22:00:51 +0800

    Update tools/server_setting.py function server_setting   new feature:     suppot specify setting item     suppot edit&save server setting   usage     change:       add parameter: key, value
        necessary parameter: server_id
        optional parameter: key, value
    
        server_setting(server_id) -> return all the server setting data
        server_setting(server_id, key) -> only return specified key's value
        server_setting(server_id, key, value) -> edit server setting and
          save it

--- tools/server_setting.py
+++ tools/server_setting.py
@@ -1,8 +1,19 @@
-from json import load
+from json import load, dump
 
 from .run_here import run_here
 
 @run_here('..')
-def server_setting(server_id):
-	with open(f"settings/server/{server_id}.json") as server_setting_file:
-		return load(server_setting_file)
+def server_setting(server_id, key = None, value = None):
+	server_setting_file_path = f"settings/server/{server_id}.json"
+	with open(server_setting_file_path, 'r') as server_setting_file:
+		server_setting_data = load(server_setting_file)
+	
+	if key is None:
+		return server_setting_data
+	if key is not None:
+		if value is None:
+			return server_setting_data[key]
+		
+		server_setting_data[key] = value
+		with open(server_setting_file_path, 'w') as server_setting_file:
+			dump(server_setting_data, server_setting_file, indent = '\t')
