8f4a1ece1a9c039d5c6f0dc3926a14dbf4d92663
Euxcbsks <hawhaw02030746@gmail.com>
2021-03-17 17:46:24 +0800

    Update tools/server_setting.py function server_setting   new feature:     suppot add&delete key of server setting file   change:     add parameter: action(default None), valid value: add, del   syntax:     server_setting(server_id, [key, [value, [action]]])
      if action is specified, the return will be None

--- tools/server_setting.py
+++ tools/server_setting.py
@@ -2,18 +2,47 @@ from json import load, dump
 
 from .run_here import run_here
 
+__all__ = [
+	'server_setting'
+]
+
 @run_here('..')
-def server_setting(server_id, key = None, value = None):
+def server_setting(server_id, key = None, value = None, action = None):
+	def save():
+		with open(server_setting_file_path, 'w') as server_setting_file:
+			dump(server_setting_data, server_setting_file, indent = '\t')
+	
+	def update():
+		server_setting_data[key] = value
+		save()
+		
+	def delete():
+		del server_setting_data[key]
+		save()
+	
 	server_setting_file_path = f"settings/server/{server_id}.json"
+	actions = {'add': update, 'del': delete}
+	
 	with open(server_setting_file_path, 'r') as server_setting_file:
 		server_setting_data = load(server_setting_file)
 	
+	#request all server_setting_data
 	if key is None:
 		return server_setting_data
-	if key is not None:
-		if value is None:
-			return server_setting_data[key]
-		
-		server_setting_data[key] = value
-		with open(server_setting_file_path, 'w') as server_setting_file:
-			dump(server_setting_data, server_setting_file, indent = '\t')
+	
+	#request specified key's value of server_setting_data
+	if value is None:
+		return server_setting_data[key]
+	
+	#update specified key's value of server_setting_data
+	if action is None and key in server_setting_data:
+		update()
+		return
+	else:
+		raise KeyError(f"Invalid key '{key}'")
+	
+	#add or delete specified key of server_setting_data
+	if action not in actions:
+		raise ValueError(f"invalid action '{action}'")
+	
+	actions[action]()
