5c3374e0e0de11fd88040f7f6a77f848fd1960a4
Euxcbsks <hawhaw02030746@gmail.com>
2021-06-13 22:44:44 +0800

    Corresponding language file update

--- lrb_backend/event.py
+++ lrb_backend/event.py
@@ -1,9 +1,11 @@
 from discord.ext.commands import Cog
 
 import global_object
-from global_object import Inited_cog, ready
-from tools.load import load_internal, load_lang
+
+from tools.load import load_internal
 from tools.setting import ServerSetting
+from tools.token import Token
+from global_object import Inited_cog, ready
 
 class Event(Inited_cog):
 	ready()
@@ -17,20 +19,22 @@ class Event(Inited_cog):
 	@Cog.listener()
 	async def on_member_join(self, member):
 		server_id = member.guild.id
-		member_join_message = ServerSetting(server_id).request("optional.member_join_message")
+		server_setting = ServerSetting(server_id)
+		member_join_message = server_setting.request(Token("optional.member_join_message"))
 		
 		if member_join_message["able"]:
 			channel = self.bot.get_channel(member_join_message["channel"])
-			await channel.send(load_lang(server_id, "message.member_join_message").format(member = member))
+			await channel.send(server_setting.request_lang("message.member_join_message").format(member = member))
 	
 	@Cog.listener()
 	async def on_member_leave(self, member):
 		server_id = member.guild.id
-		member_leave_message = ServerSetting(server_id).request("optional.member_leave_message")
+		server_setting = ServerSetting(server_id)
+		member_leave_message = server_setting.request(Token("optional.member_leave_message"))
 		
 		if member_leave_message["able"]:
 			channel = self.bot.get_channel(member_leave_message["channel"])
-			await channel.send(load_lang(server_id, "message.member_leave_message").format(member = member))
+			await channel.send(server_setting.request_lang("message.member_leave_message").format(member = member))
 
 def setup(bot):
 	bot.add_cog(Event(bot))
--- lrb_command/server.py
+++ lrb_command/server.py
@@ -1,10 +1,11 @@
 from discord.ext.commands import command, has_permissions
 
 from global_object import Inited_cog
-from language.language import languages
+from language.language import languages, language_to_name
 from tools.load import load_lang
 from tools.prefix import request_prefix, edit_prefix
 from tools.setting import ServerSetting
+from tools.token import Token
 
 
 @has_permissions(administrator = True)
@@ -15,15 +16,15 @@ class Server(Inited_cog):
 		server_setting = ServerSetting(server_id)
 		
 		if language not in languages:
-			return await ctx.send(load_lang(server_id, "error.lang_not_found"))
+			return await ctx.send(server_setting.request_lang("error.lang_not_found"))
 		
-		server_lang = server_setting.request("config.language")
+		server_lang = server_setting.request(Token("config.language"))
 		
 		if language == server_lang:
-			return await ctx.send(load_lang(server_id, "error.lang_not_change"))
+			return await ctx.send(server_setting.request_lang("error.lang_not_change"))
 		
-		server_setting.edit({"config.language": language})
-		await ctx.send(load_lang(server_id, "info.server.set_lang").format(language = languages[language]))
+		server_setting.edit({Token("config.language"): language})
+		await ctx.send(server_setting.request_lang("info.server.set_lang").format(language = language_to_name[language]))
 	
 	@command()
 	async def set_prefix(self, ctx, prefix):
