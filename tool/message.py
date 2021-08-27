2f5509b965621a777b57ef1517ecc0bcdc2a2ae7
Euxcbsks <hawhaw02030746@gmail.com>
2021-08-27 17:21:40 +0800

    New: send_warning

--- tool/message.py
+++ tool/message.py
@@ -1,5 +1,8 @@
 async def send_error(ctx, message):
 	return await ctx.send(message, delete_after=5)
 
+async def send_warning(ctx, message):
+	return await ctx.send(message, delete_after=10)
+
 async def send_info(ctx, message):
 	return await ctx.send(message, delete_after=3)
