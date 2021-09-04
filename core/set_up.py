af15b44ef60b77c4a8682151ba5d35498d23d963
Euxcbsks <hawhaw02030746@gmail.com>
2021-09-04 17:35:48 +0800

    Rename, New: start_up->set_up, new func set_up_bot

---
+++ core/set_up.py
@@ -0,0 +1,30 @@
+from json import load
+
+from discord.ext.commands import Bot
+
+def get_loading_message() -> list:
+	with open("loading_message.json", "r", encoding="UTF-8") as f:
+		return load(f)
+
+def get_bot_config(key=None):
+	with open("bot-config.json", "r", encoding="UTF-8") as f:
+		bot_config: dict = load(f)
+	
+	status = "stable" if bot_config["stable"] else "indev"
+	bot_config["token"] = bot_config["token"][status]
+	bot_config["status"] = status
+	
+	if key:
+		return bot_config.get(key)
+	return bot_config
+
+def set_up_bot(command_prefix, *, intent=None):
+	bot_config = get_bot_config()
+	
+	print("Setting up bot")
+	
+	lrb = Bot(command_prefix=command_prefix, owner_id=bot_config.get("owner_id"), intent=intent)
+	setattr(lrb, "stable", bot_config["stable"])
+	setattr(lrb, "status", bot_config["status"])
+	setattr(lrb, "is_running", False) # for event on_ready judge is ready or reconnect
+	return lrb
