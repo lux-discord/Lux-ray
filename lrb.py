a444cae543610cf201a8bca0126020e22d6f55d4
Euxcbsks <hawhaw02030746@gmail.com>
2021-09-04 17:37:14 +0800

    Improve: simplify structure

--- lrb.py
+++ lrb.py
@@ -1,36 +1,17 @@
 from discord import Intents
-from discord.ext.commands import Bot
 
-from core.cog import cog_folder_loader, cog_folders
+from core.cog import default_cog_folders, load_cog_folders
 from core.prefix import get_prefix
-from core.start_up import get_bot_data, load_start_up_message
+from core.set_up import set_up_bot, get_loading_message, get_bot_config
 
-# set up bot
-start_up_message = load_start_up_message()
-print(start_up_message["set_up"])
-stable = False
-status = "stable" if stable else "indev"
-bot_data = get_bot_data(status)
-intent = Intents.all()
-lrb = Bot(command_prefix=get_prefix, owner_id=bot_data["owner"], intents=intent)
-setattr(lrb, "status", status)
-setattr(lrb, "is_running", False)
+lrb = set_up_bot(get_prefix, intent=Intents.all())
+load_cog_folders(lrb, default_cog_folders, loading_messages=get_loading_message())
 
-# load cog
-load_cog_message_keys = [
-	"load_be",
-	"load_cmd",
-	"load_ext"
-]
-
-for folder, message_key in zip(cog_folders, load_cog_message_keys):
-	print(start_up_message[message_key])
-	cog_folder_loader(lrb, folder)
-
-# start bot
-print(start_up_message["start_bot"])
-if stable:
+# import keep_alive if bot is stable
+if lrb.stable:
 	from keep_alive import keep_alive
 	keep_alive()
 
-lrb.run(bot_data["token"])
+bot_token = get_bot_config("token")
+print("Starting bot")
+lrb.run(bot_token)
