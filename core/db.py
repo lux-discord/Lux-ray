accb1e4d662fceefe7f87c67e2ca30d3ec3d2ca4
Euxcbsks <hawhaw02030746@gmail.com>
2021-09-04 17:38:44 +0800

    Add: default server_coll and extension_coll

--- core/db.py
+++ core/db.py
@@ -3,3 +3,6 @@ from pymongo import MongoClient
 client = MongoClient()
 bot_db = client["discord-bot"]
 extension_server_db = client["discord-bot-extension"]
+
+server_coll = bot_db["server"]
+extension_coll = bot_db["extension"]
