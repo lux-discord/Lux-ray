1f2206a56cb74951070dcd78d01d4634c9cce5f4
Euxcbsks <hawhaw02030746@gmail.com>
2021-09-06 20:55:18 +0800

    Improve: error msg are now easier to understand

--- exceptions.py
+++ exceptions.py
@@ -57,5 +57,27 @@ class InvalidExtension(LRBError):
 		return f"Invalid extension '{self.args[0]}'"
 
 class InvalidToken(InvalidArgument):
+	def __init__(self, *args: object, **kargs) -> None:
+		super().__init__(*args)
+		self.kargs = kargs
+	
 	def __str__(self) -> str:
-		return f"Invalid token: '{self.args[0]}'"
+		file_name = __file__.split('\\')[-1][:-2]
+		self_name = self.__class__.__name__
+		token_str = self.kargs.get("token", self.kargs.get("token_str", None))
+		invalid_key = self.kargs.get("key")
+		delimiter = self.kargs.get("delimiter")
+		
+		if not token_str:
+			raise ValueError("No token(str) input")
+		
+		if not invalid_key:
+			raise ValueError("No invalid key input")
+		
+		if not delimiter:
+			raise ValueError("No delimiter input")
+		
+		return f"{token_str}\n{' '*(len(file_name)+len(self_name)+3+len(delimiter))}{'^'*len(invalid_key)}"
+		# why +3 when repeating " ":
+		# 1 for '.' betwean file name and self name
+		# 2 for ': ' after self name
