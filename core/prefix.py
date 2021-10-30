2154e711f8d70c1840a068677ea69dcd829489cc
Euxcbsks <56812006+Euxcbsks@users.noreply.github.com>
2021-10-30 22:34:18 +0800

    Rewrite prefix system

--- core/prefix.py
+++ core/prefix.py
@@ -1,56 +1,18 @@
-from exceptions import InvalidPrefix
-from pymongo.errors import CollectionInvalid
-
-from .db import bot_db
-
-prefixes_coll = bot_db["prefixes"]
 prefix_cache = {}
-default_prefix_delimiter = ";"
-default_prefixes = {
-	"stable": "l" + default_prefix_delimiter,
-	"indev": "r" + default_prefix_delimiter
-}
 
-def get_prefix(bot, message):
+def get_server_prefix(bot, message):
+	"""
+	Get prefix by server(guild) id
+	
+	get from cache, db or default value when both don't set
+	"""
 	server_id: int = message.guild.id
-	bot_status = bot.status
 	
 	try:
 		prefix = prefix_cache[server_id]
 	except KeyError:
-		if not (prefix := find_prefix(server_id, bot_status)):
-			prefix = insert_prefixes(server_id)[bot_status]
+		if not (prefix := bot.db.find_prefix(server_id=server_id)):
+			# when bot enter a new server
+			prefix = bot.db.insert_prefix(server_id=server_id)
 	
-	return prefix + " ", prefix
-
-def insert_prefixes(server_id, prefixes: dict=None):
-	if not prefixes:
-		prefixes_coll.insert_one({
-			"_id": server_id,
-			"prefixes": default_prefixes
-		})
-		
-		return default_prefixes
-	
-	prefixes_coll.insert_one({
-		"_id": server_id,
-		"prefixes": prefixes
-	})
-	
-	return prefixes
-
-def find_prefix(server_id, bot_status):
-	if server_data := prefixes_coll.find_one({"_id": server_id}):
-		prefix = server_data["prefixes"][bot_status]
-		prefix_cache[server_id] = prefix
-		
-		return prefix
-	return None
-
-def update_prefix(server_id, status, prefix):
-	try:
-		prefixes_coll.update_one({"_id": server_id}, {"prefixes": {status: prefix}})
-		prefix_cache[server_id] = prefix
-	except CollectionInvalid:
-		# raise when stable and indev prefix are same
-		raise InvalidPrefix(prefix)
+	return prefix
