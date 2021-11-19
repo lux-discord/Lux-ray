12b5362d8dbc213e04f9158a61363e5d72d3e2a1
Euxcbsks <56812006+Euxcbsks@users.noreply.github.com>
2021-11-19 17:31:50 +0800

    Add get/send message method

--- utils/cog.py
+++ utils/cog.py
@@ -1,4 +1,6 @@
 from disnake.ext.commands import Bot, Cog
+from core.language import request_lang
+
 
 class InitedCog(Cog):
 	def __init__(self, bot: Bot) -> None:
@@ -11,3 +13,41 @@ class InitedCog(Cog):
 		self.find_server = bot.db.find_server
 		self.insert_server = bot.db.insert_server
 		self.update_server = bot.db.update_server
+	
+	def get_message(self, server_id: int, token, *, lang_dir="language", **mes_format) -> str:
+		"""
+		Parameter
+		---------
+		server_id: int
+			server's id
+		token: utils.token.Token
+			token that use to request message
+		[lang_dir]="language": str
+			language file's dirctory, default is public language dirctory
+		[**mes_format]:
+			use for format message
+		
+		Return type
+		-----------
+		str
+		"""
+		lang_code = self.get_server(server_id)["lang_code"]
+		language = request_lang(lang_dir, lang_code)
+		
+		if message := language.request_message(token) and mes_format:
+			message = message.format(**mes_format)
+		
+		return message
+	
+	async def send_info(self, ctx, token, **mes_format):
+		message = self.get_message(ctx.guild.id, token, **mes_format)
+		return await ctx.send(message, delete_after=3)
+	
+	async def send_warning(self, ctx, token, **mes_format):
+		message = self.get_message(ctx.guild.id, token, **mes_format)
+		return await ctx.send(message, delete_after=6)
+	
+	async def send_error(self, ctx, token, **mes_format):
+		message = self.get_message(ctx.guild.id, token, **mes_format)
+		return await ctx.send(message, delete_after=9)
+
