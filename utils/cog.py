62b27c53bf3729ca311068caead6562ffb6f6e8c
Euxcbsks <56812006+Euxcbsks@users.noreply.github.com>
2021-11-19 15:15:11 +0800

    Add shortcut and comment

--- utils/cog.py
+++ utils/cog.py
@@ -2,4 +2,12 @@ from disnake.ext.commands import Bot, Cog
 
 class InitedCog(Cog):
 	def __init__(self, bot: Bot) -> None:
+		# Basic attr
 		self.bot = bot
+		
+		# Shortcuts
+		self.db = bot.db
+		self.get_server = bot.db.get_server
+		self.find_server = bot.db.find_server
+		self.insert_server = bot.db.insert_server
+		self.update_server = bot.db.update_server
