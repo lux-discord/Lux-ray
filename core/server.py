d3872adcacfec767a8d630c1408395053ba41a5f
Euxcbsks <hawhaw02030746@gmail.com>
2022-02-26 04:13:50 +0000

    Rename id to server_id

--- core/server.py
+++ core/server.py
@@ -3,7 +3,7 @@ from exceptions import InvalidPrefix
 
 class ServerBase():
 	def __init__(self, server_data: dict) -> None:
-		self.id = server_data["id"] if not (id := server_data.get("_id")) else id
+		self.id = server_data["id"] if not (server_id := server_data.get("_id")) else server_id
 		self.lang_code = server_data["lang_code"]
 
 class Server(ServerBase):
