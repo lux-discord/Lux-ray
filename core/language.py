be665d3767af7e0cff10d6fac7a2dc30e04383b2
Euxcbsks <hawhaw02030746@gmail.com>
2022-02-26 04:10:27 +0000

    Add comment

--- core/language.py
+++ core/language.py
@@ -8,6 +8,7 @@ from utils.token import Token
 GLOBAL_LANGUAGE_DIR = Path("language")
 GLOBAL_SUPPORT_LANGUAGE = {lang_file.name for lang_file in GLOBAL_LANGUAGE_DIR.iterdir()}
 
+# For custom language file
 def get_support_language(lang_dir: Path):
 	return {lang_file.name for lang_file in lang_dir.iterdir()}
 
