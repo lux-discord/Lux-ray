223fe352cae4ee6509886fad736cac5d8c219bea
Euxcbsks <hawhaw02030746@gmail.com>
2022-02-26 04:00:14 +0000

    Add/Modify doc, misc(below)
    * Remove import Server
    * Add some comment
    * Reanme some variable
    * insert_prefix must pass in prefix argument now
    * filter that use server_id use self._server_id_filter now

--- core/db.py
+++ core/db.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-from .server import Server
+
 
 class MongoDB():
 	def __init__(self, db_host, *, db_port=None) -> None:
@@ -14,64 +14,118 @@ class MongoDB():
 		"""
 		return {"_id": server_id}
 	
+	# Prefix
 	def get_prefix(self, server_id: int):
+		"""
+		Alias of find_prefix
+		"""
 		return self.find_prefix(server_id)
 	
 	def find_prefix(self, server_id: int):
-		if prefix_doc := self.prefix.find_one({"_id": server_id}):
-			prefix = prefix_doc["prefix"]
-			self.prefix_cache[server_id] = prefix
-			return prefix
-		
-		return None
-	
-	def insert_prefix(self, server_id: int, *, prefix: str=None):
 		"""
-		Insert prefix to db by server id
+		Find prefix from db by server_id
 		
 		Argument
 		--------
 		server_id: int
-			server's id
-		prefix: str
-			default "l;" if not pass in
+			server ID
+		
+		Return
+		------
+		The server's prefix or None if not found
 		
 		Return Type
-		------
-		prefix that pass in or default value
+		-----------
+		Optinoal[str]
 		"""
-		prefix = prefix if prefix else "l;"
+		if doc := self.prefix.find_one(self._server_id_filter(server_id)):
+			return doc["prefix"]
+		
+		return None
+	
+	def insert_prefix(self, server_id: int, prefix: str):
+		"""
+		Insert prefix by server id
+		
+		Argument
+		--------
+		server_id: int
+			server id
+		prefix: str
+			server prefix
+		
+		Return
+		------
+		The prefix that pass in
+		
+		Return Type
+		-----------
+		str
+		"""
 		self.prefix.insert_one({
 			"_id": server_id,
 			"prefix": prefix,
 		})
-		self.prefix_cache[server_id] = prefix
 		
 		return prefix
 	
 	def update_prefix(self, server_id: int, prefix: str):
+		"""
+		Update prefix by server ID
+		
+		Argument
+		--------
+		server_id: int
+			server id
+		prefix: str
+			new server prefix
+		
+		Return
+		------
+		prefix that pass in
+		
+		Return Type
+		-----------
+		str
+		"""
 		self.prefix.update_one(
-			filter={"_id": server_id},
+			filter=self._server_id_filter(server_id),
 			update={"$set": {"prefix": prefix}}
 		)
 	
+	# Server
 	def get_server(self, server_id: int):
+		"""
+		Alias of find_server
+		"""
 		return self.find_server(server_id)
 	
 	def find_server(self, server_id: int):
-		if server_doc := self.server.find_one({"_id": server_id}):
-			return server_doc
+		"""
+		Unfinished...
+		"""
+		if doc := self.server.find_one(self._server_id_filter(server_id)):
+			return doc
 		
 		return None
 	
 	def insert_server(self, server_id: int, server_data: dict=None):
+		"""
+		Unfinished...
+		"""
 		self.server.insert_one({
 			"_id": server_id,
 			"lang_code": "en"
 		} if not server_data else server_data)
 	
 	def update_server(self, server_id: int, update: dict):
+		"""
+		Unfinished...
+		
+		TO DO:
+		1. "update" use custom class instead
+		"""
 		self.server.update_one(
-			filter={"_id": server_id},
+			filter=self._server_id_filter(server_id),
 			update={"$set": update}
 		)
