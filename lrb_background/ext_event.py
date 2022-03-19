a81d553cbe356a5910b908a75520e1e90530997f
Euxcbsks <hawhaw02030746@gmail.com>
2022-03-19 03:19:48 +0000

    Use Cog method instead Server instance

--- lrb_background/ext_event.py
+++ lrb_background/ext_event.py
@@ -1,4 +1,3 @@
-from core import Server
 from disnake.ext.commands import Cog
 from core.cog import GeneralCog
 
@@ -7,7 +6,7 @@ class ExtEvent(GeneralCog):
 	async def on_command_error(self, ctx, error):
 		# command not exist
 		if not ctx.command:
-			return await Server(ctx).send_error("error.invalid_command.command_not_exist", command_name=ctx.invoked_with)
+			return await self.send_error(ctx, self.token("error.invalid_command.command_not_exist"), command_name=ctx.invoke_with)
 		
 		if ctx.command.has_error_handler():
 			return
