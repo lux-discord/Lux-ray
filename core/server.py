fe70b63b665f9a684a895a8b88330c716b7b2986
Euxcbsks <hawhaw02030746@gmail.com>
2022-03-19 03:11:36 +0000

    PREVIEW Server

--- core/server.py
+++ core/server.py
@@ -1,12 +1,13 @@
-from exceptions import InvalidPrefix
-
-
 class ServerBase():
 	def __init__(self, server_data: dict) -> None:
 		self.id = server_data["id"] if not (server_id := server_data.get("_id")) else server_id
 		self.lang_code = server_data["lang_code"]
 
 class Server(ServerBase):
-	"""Preview"""
+	"""
+	Unfinished...
+	"""
 	def __init__(self, server_data: dict) -> None:
 		super().__init__(server_data)
+		
+		self.prefix = server_data["prefix"]
