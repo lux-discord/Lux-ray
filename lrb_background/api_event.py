19a84ac0010446fe6dda82b7de726dc3b59dab70
Euxcbsks <hawhaw02030746@gmail.com>
2022-03-19 03:16:33 +0000

    Rewrite auto_role process

--- lrb_background/api_event.py
+++ lrb_background/api_event.py
@@ -13,10 +13,9 @@ class ApiEvent(GeneralCog):
 	@Cog.listener()
 	async def on_member_join(self, member: Member):
 		async def auto_role():
-			server_data: dict = self.bot.db.find_one({"_id": member.guild.id})
-			auto_roles: list = server_data["roles"]["auto_roles"]
+			server_data: dict = self.db.find_one({"_id": member.guild.id})
 			
-			if auto_roles:
+			if auto_roles := server_data["roles"]["auto_roles"]:
 				server_roles = member.guild.roles
 				_ = [await member.add_roles(get(server_roles, name=role)) for role in auto_roles]
 		
