2e79cafdb293da81a174e07c459973a65a26325d
Euxcbsks <hawhaw02030746@gmail.com>
2022-03-30 00:50:59 +0000

    Update bot.get_prefix function with new design

--- core/prefix.py
+++ core/prefix.py
@@ -1,7 +1,13 @@
-def get_prefix(bot, message):
+from core.data import PrefixData
+from core.config import get_default_prefix
+
+
+async def get_prefix(bot, message):
 	server_id = message.guild.id
 	
-	if not (prefix := bot.db.get_prefix(server_id)):
-		prefix = bot.db.insert_prefix(server_id)
+	if not (prefix := await bot.db.find_prefix(server_id)):
+		default_data = PrefixData(_id=server_id, prefix=get_default_prefix(bot.config, bot.mode))
+		await bot.db.insert_prefix(default_data)
+		prefix = default_data.prefix
 	
 	return prefix + " ", prefix
