9fb87583f1f45f1e74572cc111ac9c7ee45b2238
Euxcbsks <hawhaw02030746@gmail.com>
2022-03-30 03:49:09 +0000

    Rewrite Server
    * Server no longer inherit from ServerBase, since it is deleted
    * New method `update`, return ServerData base on `self.items`, `update`

--- core/server.py
+++ core/server.py
@@ -1,13 +1,23 @@
-class ServerBase():
-	def __init__(self, server_data: dict) -> None:
-		self.id = server_data["id"] if not (server_id := server_data.get("_id")) else server_id
-		self.lang_code = server_data["lang_code"]
+from core.data import ServerData
 
-class Server(ServerBase):
-	"""
-	Unfinished...
-	"""
-	def __init__(self, server_data: dict) -> None:
-		super().__init__(server_data)
+
+class Server():
+	def __init__(self, server_data: ServerData) -> None:
+		self.items = server_data.items
+		self.id = server_data.id
+		self.lang_code = server_data.lang_code
+		self.role = server_data.role
+	
+	def update(self, **update):
+		"""
+		Generatr a ServerData with self.items and gived update
 		
-		self.prefix = server_data["prefix"]
+		Return
+		------
+		A ServerData instance base on self.items and update
+		
+		Return type
+		-----------
+		`core.data.ServerData`
+		"""
+		return ServerData.from_items(self.items | update)
