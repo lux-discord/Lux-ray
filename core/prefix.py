eb2026fce007d095c723f2320192d9a5e1d60c28
Euxcbsks <hawhaw02030746@gmail.com>
2022-04-11 10:30:38 +0000

    Fix issue #17

--- core/prefix.py
+++ core/prefix.py
@@ -1,13 +1,17 @@
-from core.data import PrefixData
 from core.config import get_default_prefix
+from core.data import PrefixData
+from exceptions import ConfigInvalid
 
 
 async def get_prefix(bot, message):
 	server_id = message.guild.id
 	
 	if not (prefix := await bot.db.find_prefix(server_id)):
-		default_data = PrefixData(_id=server_id, prefix=get_default_prefix(bot.config, bot.mode))
-		await bot.db.insert_prefix(default_data)
-		prefix = default_data.prefix
+		if default_prefix := get_default_prefix(bot.config, bot.mode):
+			default_data = PrefixData(_id=server_id, prefix=default_prefix)
+			await bot.db.insert_prefix(default_data)
+			prefix = default_data.prefix
+		else:
+			raise ConfigInvalid(f"prefix.{bot.mode}.prefix", "None")
 	
 	return prefix + " ", prefix
