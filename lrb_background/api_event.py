a9bf35e1b6294bf167cef5a5625ea282223a06c5
Euxcbsks <hawhaw02030746@gmail.com>
2022-04-11 09:53:51 +0000

    Comment `on_member_join` event

--- lrb_background/api_event.py
+++ lrb_background/api_event.py
@@ -1,4 +1,4 @@
-from disnake import Member
+#from disnake import Member
 from disnake.ext.commands.cog import Cog
 
 from core.cog import GeneralCog
@@ -13,14 +13,16 @@ class ApiEvent(GeneralCog):
 	async def on_ready(self):
 		print("Bot is ready")
 	
+	"""
+	# [Bug] bot won't recived event
 	@Cog.listener()
 	async def on_member_join(self, member: Member):
-		# [Bug] bot won't recived event
 		server = await self.get_server(member.guild.id)
 		
 		if auto_roles := server.role["auto_role"]:
 			roles = [member.guild.get_role(role_id) for role_id in auto_roles]
 			await member.add_roles(*roles)
+	"""
 
 def setup(bot):
 	bot.add_cog(ApiEvent(bot))
