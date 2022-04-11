b2c63da13057ca4e17091c47ae16a1f0717a6049
Euxcbsks <hawhaw02030746@gmail.com>
2022-04-11 09:54:30 +0000

    Add keyword reply function

--- lrb_background/api_event.py
+++ lrb_background/api_event.py
@@ -13,6 +13,23 @@ class ApiEvent(GeneralCog):
 	async def on_ready(self):
 		print("Bot is ready")
 	
+	@Cog.listener()
+	async def on_message(self, message):
+		if message.author.id == self.bot.user.id:
+			return
+		
+		def keyword_reply(message_content: str):
+			# Maybe will use command to set keyword and reply in future?
+			keyword_to_reply = {
+				"YABE": "https://tenor.com/view/shirakami-fubuki-hololive-yabe-vtuber-gif-19227770",
+				"窩不知道": "https://memeprod.sgp1.digitaloceanspaces.com/user-wtf/1622039006854.jpg"
+			}
+			
+			return keyword_to_reply[message_content] if message_content in keyword_to_reply else None
+		
+		if reply := keyword_reply(message.content):
+			await message.channel.send(reply)
+	
 	"""
 	# [Bug] bot won't recived event
 	@Cog.listener()
