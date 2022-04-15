13a4e01569ede406133f69f6aa66be545026ae4f
Euxcbsks <hawhaw02030746@gmail.com>
2022-04-15 06:11:13 +0000

    Part of commit `a16e5edc`

--- core/bot.py
+++ core/bot.py
@@ -5,7 +5,7 @@ from core.config import get_db_client
 
 class LuxRay(Bot):
 	def __init__(self, *args, config, mode, **kargs):
-		self.db = get_db_client(config, mode)
+		self.db = get_db_client(mode)
 		self.config = config
 		self.mode = mode
 		
