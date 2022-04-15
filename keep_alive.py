cbcbc20cf28bbe3f9b3128be093c5d20af2e5ba7
Euxcbsks <hawhaw02030746@gmail.com>
2022-04-15 17:28:11 +0000

    Use `PORT` in env var first

--- keep_alive.py
+++ keep_alive.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from threading import Thread
+from os import getenv
 
 app = Flask('')
 
@@ -8,7 +9,7 @@ def main():
 	return 'Bot is aLive!'
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=getenv("PORT", 8080))
 
 def keep_alive():
     server = Thread(target=run)
