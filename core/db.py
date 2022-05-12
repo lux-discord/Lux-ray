f1d2431b2a3bc6e3bb1b44edb33937c54673e11f
Euxcbsks <hawhaw02030746@gmail.com>
2022-05-12 12:32:12 +0800

    Add type hint

--- core/db.py
+++ core/db.py
@@ -1,17 +1,31 @@
+from typing import TYPE_CHECKING
+
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from core.data import PrefixData, ServerData
+
+if TYPE_CHECKING:
+    from typing import Optional
+
+    from pymongo.database import Database
+    from pymongo.results import DeleteResult, InsertOneResult, UpdateResult
+
+
+class DataProfile:
+    def __init__(self, category: str, filter: dict) -> None:
+        self.category = category
+        self.filter = filter
 
 
 class MongoDB:
     def __init__(self, db_host, *, db_port=None) -> None:
         self.client = AsyncIOMotorClient(host=db_host, port=db_port)
-        self.bot_db = self.client["discord-bot"]
+        self.bot_db: Database = self.client["discord-bot"]
         self.server = self.bot_db["server"]
         self.prefix = self.bot_db["prefix"]
 
     # Prefix
-    async def find_prefix(self, server_id: int):
+    async def find_prefix(self, server_id: int) -> Optional[str]:
         """
         Find prefix by server id
 
@@ -31,7 +45,7 @@ class MongoDB:
         doc = await self.prefix.find_one({"_id": server_id})
         return doc["prefix"] if doc else None
 
-    async def insert_prefix(self, prefix_data: PrefixData):
+    async def insert_prefix(self, prefix_data: PrefixData) -> InsertOneResult:
         """
         Insert prefix
 
@@ -46,7 +60,7 @@ class MongoDB:
         """
         return await self.prefix.insert_one(prefix_data.to_dict())
 
-    async def update_prefix(self, update: PrefixData):
+    async def update_prefix(self, update: PrefixData) -> UpdateResult:
         """
         Update prefix
 
@@ -62,7 +76,7 @@ class MongoDB:
         _update = {"$set": {"prefix": update.prefix}}
         return await self.prefix.update_one({"_id": update.id}, _update)
 
-    async def delete_prefix(self, server_id: int):
+    async def delete_prefix(self, server_id: int) -> DeleteResult:
         """
         Delete prefix
 
@@ -78,7 +92,7 @@ class MongoDB:
         return await self.prefix.delete_one({"_id": server_id})
 
     # Server
-    async def find_server(self, server_id: int):
+    async def find_server(self, server_id: int) -> Optional[dict]:
         """
         Find server by server id
 
@@ -97,7 +111,7 @@ class MongoDB:
         """
         return await self.server.find_one({"_id": server_id})
 
-    async def insert_server(self, server_data: ServerData):
+    async def insert_server(self, server_data: ServerData) -> InsertOneResult:
         """
         Argument
         --------
@@ -113,7 +127,7 @@ class MongoDB:
         """
         return await self.server.insert_one(server_data.to_dict())
 
-    async def update_server(self, update: ServerData):
+    async def update_server(self, update: ServerData) -> UpdateResult:
         """
         Update server
 
@@ -129,7 +143,7 @@ class MongoDB:
         _update = {"$set": update.to_dict()}
         return await self.server.update_one({"_id": update.id}, _update)
 
-    async def delete_server(self, server_id: int):
+    async def delete_server(self, server_id: int) -> DeleteResult:
         """
         Delete server data
 
