a44a8a6501ca0fe7e59cd4ee95b874e6eec861fc
Euxcbsks <hawhaw02030746@gmail.com>
2022-05-15 14:45:53 +0800

    Fix parse error when get config from env

--- core/setup.py
+++ core/setup.py
@@ -16,7 +16,12 @@ def get_bot_config(config_path) -> dict:
     if Path(config_path).exists():
         with open(config_path, "rb") as f:
             return load(f)
-    return loads(getenv("CONFIG"))
+    # When the environment variable contains special characters like newlines, tabs...etc
+    # getenv will return a string that has been replaced with escape character format (eg: "\\n", "\\t"...)
+    # to make it easier for users to read
+    # So we need to replace it with the original special character so that tomli can parse it correctly
+    config_data = getenv("CONFIG").replace("\\n", "\n").replace("\\t", "\t")
+    return loads(config_data)
 
 
 def setup_bot(config, mode):
