12b63478933219f0ea146e865deec4906a502d73
Euxcbsks <hawhaw02030746@gmail.com>
2022-05-18 17:31:44 +0800

    Add type hint

--- core/checks.py
+++ core/checks.py
@@ -1,14 +1,14 @@
-from disnake import Permissions
-from disnake.ext.commands import MissingPermissions
+from disnake import Permissions, TextChannel
+from disnake.ext.commands import Context, MissingPermissions
 
 
-def has_channel_permissions(ctx, channel, **perms: bool):
+def has_channel_permissions(ctx: Context, channel: TextChannel, **perms: bool):
     invalid = set(perms) - set(Permissions.VALID_FLAGS)
 
     if invalid:
         raise TypeError(f"Invalid permission(s): {', '.join(invalid)}")
 
-    permissions = channel.permission_for(ctx.author)
+    permissions = channel.permissions_for(ctx.author)
     missing = [
         perm for perm, value in perms.items() if getattr(permissions, perm) != value
     ]
