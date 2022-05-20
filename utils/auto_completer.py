a22b330768c7b8460aa1ced13c63d0cf51991e18
Euxcbsks <hawhaw02030746@gmail.com>
2022-05-20 13:04:33 +0800

    Add general choose generater

--- utils/auto_completer.py
+++ utils/auto_completer.py
@@ -1,6 +1,31 @@
+from typing import TYPE_CHECKING
+
 from disnake import ApplicationCommandInteraction
 
+if TYPE_CHECKING:
+    from typing import Iterable, Mapping
+
 BOOL_CHOOSES = ["true", "false"]
+
+
+def choose_list_generater(chooses: "Iterable[str]", user_input: str):
+    return (
+        list(chooses)
+        if not user_input
+        else [choose for choose in chooses if user_input.lower() in choose]
+    )
+
+
+def choose_mapping_generater(chooses: "Mapping[str, str]", user_input: str):
+    return (
+        dict(chooses)
+        if not user_input
+        else {
+            choose: value
+            for choose, value in chooses.items()
+            if user_input.lower() in choose
+        }
+    )
 
 
 async def bool_autocom(inter: ApplicationCommandInteraction, user_input: str = None):
