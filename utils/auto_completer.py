95213d9a698bf69040d08c7fc412a910a9b2b0a8
Euxcbsks <hawhaw02030746@gmail.com>
2022-05-20 20:51:10 +0800

    New `cog_file_autocom`, `cog_folder_autocom`

--- utils/auto_completer.py
+++ utils/auto_completer.py
@@ -7,6 +7,8 @@ from core.language import GLOBAL_SUPPORT_LANGUAGE
 if TYPE_CHECKING:
     from typing import Iterable, Mapping
 
+    from core.bot import LuxRay
+
 BOOL_CHOOSES = ["true", "false"]
 
 
@@ -41,3 +43,30 @@ async def lang_code_autocom(
     support_language: "Iterable[str]" = GLOBAL_SUPPORT_LANGUAGE,
 ):
     return choose_list_generater(support_language, user_input)
+
+
+async def cog_file_autocom(
+    inter: ApplicationCommandInteraction, user_input: str = None
+):
+
+    bot: "LuxRay" = inter.bot
+    config = bot.config
+
+    return (
+        config.all_cog_files
+        if not user_input
+        else [file for file in config.all_cog_files if user_input.lower() in file]
+    )
+
+
+async def cog_folder_autocom(
+    inter: ApplicationCommandInteraction, user_input: str = None
+):
+    bot: "LuxRay" = inter.bot
+    config = bot.config
+
+    return (
+        config.cog_folders
+        if not user_input
+        else [folder for folder in config.cog_folders if user_input.lower() in folder]
+    )
