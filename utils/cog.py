cb1f2a74e27fcd197e02e185c716e268499dd4da
Euxcbsks <hawhaw02030746@gmail.com>
2022-05-20 19:27:33 +0800

    Add `reload_file`, `reload_folder` method

--- utils/cog.py
+++ utils/cog.py
@@ -6,24 +6,32 @@ class CogManager:
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
         self.indent = "    "
-        self.file_message = f"{self.indent}File: "
-        self.folder_message = f"{self.indent}Folder: "
+        self.file = f"file:"
+        self.folder = f"folder:"
 
     def load_file(self, name: str):
-        print(self.file_message + name)
+        print(f"Load {self.file} {name}")
         self.bot.load_extension(name)
 
     def load_folder(self, path: str):
-        print(self.folder_message + path)
+        print(f"Load {self.folder} {path}")
         self.bot.load_extensions(path)
 
     def unload_file(self, name: str):
-        print(self.file_message + name)
+        print(f"Unload {self.file} {name}")
         self.bot.unload_extension(name)
 
     def unload_folder(self, path: str):
-        print(self.folder_message + path)
+        print(f"Unload {self.folder} {path}")
         [self.bot.unload_extension(cog) for cog in search_directory(path)]
+
+    def reload_file(self, name: str):
+        print(f"Reload {self.file} {name}")
+        self.bot.reload_extension(name)
+
+    def reload_folder(self, path: str):
+        print(f"Reload {self.folder} {path}")
+        [self.bot.reload_extension(cog) for cog in search_directory(path)]
 
     def load(self, *, files: str = None, folders: str = None):
         print("Loading cog files and folders...")
@@ -47,9 +55,7 @@ class CogManager:
         print("Reloading cog files and folders...")
 
         if files:
-            [self.unload_file(file) for file in files]
-            [self.load_file(file) for file in files]
+            [self.reload_file(file) for file in files]
 
         if folders:
-            [self.unload_folder(folder) for folder in folders]
-            [self.load_folder(folder) for folder in folders]
+            [self.reload_folder(file) for file in files]
