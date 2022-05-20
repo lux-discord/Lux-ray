2313e06bdc9df0c3099511334805a99319dd769c
Euxcbsks <hawhaw02030746@gmail.com>
2022-05-20 19:16:01 +0800

    Add support of unload/reload cogs

--- core/bot.py
+++ core/bot.py
@@ -2,7 +2,7 @@ from typing import TYPE_CHECKING
 
 from disnake.ext.commands import Bot
 
-from utils.cog import CogLoader
+from utils.cog import CogManager
 
 if TYPE_CHECKING:
     from core.config import Config
@@ -20,6 +20,13 @@ class LuxRay(Bot):
 
         self.config = config
         self.db = config.get_database_client()
+        self.cog_manager = CogManager(self)
 
     def load_cogs(self, cog_files: list[str] = None, cog_folders: list[str] = None):
-        CogLoader(self).load(files=cog_files, folders=cog_folders)
+        self.cog_manager.load(files=cog_files, folders=cog_folders)
+
+    def unload_cogs(self, cog_files: list[str] = None, cog_folders: list[str] = None):
+        self.cog_manager.unload(files=cog_files, folders=cog_folders)
+
+    def reload_cogs(self, cog_files: list[str] = None, cog_folders: list[str] = None):
+        self.cog_manager.reload(files=cog_files, folders=cog_folders)
